ef6f89d6b6fed9e4dbac7b963894e9a23b127ddb
jeonghyeonkwon <givejeong2468@gmail.com>
2022-10-24 13:47:02 +0900

    마크다운 까지 실습 완료

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -1,7 +1,10 @@
+from distutils import extension
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flaskext.markdown import Markdown
+
 import config
 
 naming_convention = {
@@ -46,6 +49,8 @@ def create_app():
     from .filter import format_datetime
     app.jinja_env.filters['datetime'] = format_datetime
 
+    # 마크다운
+    Markdown(app, extensions=['nl2br','fenced_code'])
     return app
 
 
